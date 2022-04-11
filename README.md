786890513627ef83fd20115e22fde1537e235e1d
James-Livesey <jamesliveg@gmail.com>
2022-04-11 20:32:05 +0100

    Add basics of bare website

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Website
 The LiveG website, at liveg.tech.
+
+Licenced by the [LiveG Open-Source Licence](LICENCE.md).
---
+++ src/script.js
@@ -0,0 +1,10 @@
+/*
+    LiveG Website
+
+    Copyright (C) LiveG. All Rights Reserved.
+
+    https://liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+import * as $g from "https://livegtech.github.io/Adapt-UI/src/adaptui.js";
