5e39fe38b999a9ad440ee7b82279752dbe1a6e03
James-Livesey <jamesliveg@gmail.com>
2022-04-12 19:41:23 +0100

    Add localisation

--- src/script.js
+++ src/script.js
@@ -10,7 +10,31 @@
 import * as $g from "https://livegtech.github.io/Adapt-UI/src/adaptui.js";
 
 $g.waitForLoad().then(function() {
+    return $g.templates.apply();
+}).then(function() {
+    return $g.l10n.selectLocaleFromResources({
+        "en_GB": "locales/en_GB.json",
+        "fr_FR": "locales/fr_FR.json"
+    }, "en_GB", {}, $g.core.parameter("lang") || $g.l10n.getSystemLocaleCode());
+}).then(function(locale) {
+    window._ = function() {
+        return locale.translate(...arguments);
+    };
+
+    $g.l10n.translateApp(locale);
+}).then(function() {
     $g.sel(".nav_openMenu").on("click", function() {
         $g.sel("aside").asideOpen();
+    });
+
+    $g.sel(".l10n_openMenu").on("click", function() {
+        $g.sel(".l10n_menu").setStyle("display", "block");
+        $g.sel(".l10n_menu").menuOpen();
+    });
+
+    $g.sel(".l10n_menu button").on("click", function(event) {
+        var locale = event.target.getAttribute("data-locale");
+
+        window.location.href = `${window.location.href.split("?")[0]}?lang=${encodeURIComponent(locale)}`;
     });
 });
