2af41a88bafbeb1e7c5102d0aabf77449b72f0e2
James-Livesey <jamesliveg@gmail.com>
2022-04-13 18:19:42 +0100

    Add Developer page, translations and lib links to opensoucre.liveg.tech

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@
     Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
 */
 
-import * as $g from "https://livegtech.github.io/Adapt-UI/src/adaptui.js";
+import * as $g from "https://opensource.liveg.tech/Adapt-UI/src/adaptui.js";
 
 $g.waitForLoad().then(function() {
     return $g.templates.apply();
@@ -15,14 +15,18 @@ $g.waitForLoad().then(function() {
     return $g.l10n.selectLocaleFromResources({
         "en_GB": "/locales/en_GB.json",
         "fr_FR": "/locales/fr_FR.json"
-    }, "en_GB", {}, $g.core.parameter("lang") || $g.l10n.getSystemLocaleCode());
+    }, "en_GB", {
+        "fr_FR": "en_GB"
+    }, localStorage.getItem("liveg_website_lang") || $g.core.parameter("lang") || $g.l10n.getSystemLocaleCode());
 }).then(function(locale) {
     window._ = function() {
         return locale.translate(...arguments);
     };
 
     $g.l10n.translateApp(locale);
-}).then(function() {
+
+    $g.sel("body").removeAttribute("hidden");
+
     if ($g.sel("title").hasAttribute("data-page")) {
         $g.sel("title").setText(_("title_page", {page: _($g.sel("title").getAttribute("data-page"))}));
     }
@@ -38,6 +42,8 @@ $g.waitForLoad().then(function() {
 
     $g.sel(".l10n_menu button").on("click", function(event) {
         var locale = event.target.getAttribute("data-locale");
+
+        localStorage.setItem("liveg_website_lang", locale);
 
         window.location.href = `${window.location.href.split("?")[0]}?lang=${encodeURIComponent(locale)}`;
     });
