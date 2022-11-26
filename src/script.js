dd2798f3da8ceceee9f13d1b3890b88b9eef207b
James-Livesey <jamesliveg@gmail.com>
2022-11-26 18:58:29 +0000

    Ensure cache invalidation for translation files

--- src/script.js
+++ src/script.js
@@ -13,9 +13,9 @@ $g.waitForLoad().then(function() {
     return $g.templates.apply();
 }).then(function() {
     return $g.l10n.selectLocaleFromResources({
-        "en_GB": "/locales/en_GB.json",
-        "fr_FR": "/locales/fr_FR.json",
-        "zh_CN": "/locales/zh_CN.json"
+        "en_GB": "/locales/en_GB.json?v=1",
+        "fr_FR": "/locales/fr_FR.json?v=1",
+        "zh_CN": "/locales/zh_CN.json?v=1"
     }, "en_GB", {
         "fr_FR": "en_GB",
         "zh_CN": "en_GB"
