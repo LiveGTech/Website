55c53de2107391f7a2497d96ebfd950e4b0caba2
James-Livesey <jamesliveg@gmail.com>
2023-11-19 12:07:42 +0000

    Add editor and other layout parts for bday playground

--- src/components/playground/playground.js
+++ src/components/playground/playground.js
@@ -8,12 +8,42 @@
 */
 
 import * as astronaut from "https://opensource.liveg.tech/Adapt-UI/astronaut/astronaut.js";
+import * as typeset from "https://opensource.liveg.tech/Typeset-Engine/src/typeset.js";
 
 var c = astronaut.components;
 
+typeset.init();
+
 export var Playground = astronaut.component("Playground", function() {
-    // TODO: Implement playground
-    return c.Container() (
-        c.Paragraph() ("Playground goes here")
+    return c.Container({
+        attributes: {
+            "aui-stack": "horizontal"
+        }
+    }) (
+        c.Container({
+            attributes: {
+                "aui-stack": "vertical"
+            }
+        }) (
+            c.Card({
+                styles: {
+                    "flex-basis": "unset",
+                    "margin-bottom": "0"
+                }
+            }) (
+                c.Paragraph() ("Challenge text goes here")
+            ),
+            c.Container (
+                typeset.CodeEditor({
+                    language: "javascript",
+                    code: `console.log("Hello, world!");`
+                }) ()
+            )
+        ),
+        c.Card({
+            styles: {
+                "margin-bottom": "0"
+            }
+        }) ()
     );
 });
